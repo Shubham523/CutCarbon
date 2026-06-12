2037f248dabd9d8148f181177fe9400362cd34c7
Shubham523 <ss4796512@gmail.com>
2026-06-12 20:02:03 +0530

    security: move Firebase config to env vars, remove hardcoded secrets

--- firebase.js
+++ firebase.js
@@ -4,13 +4,13 @@ import {getFirestore} from "firebase/firestore";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "REDACTED",
-  authDomain: "carbonemissionanalysis.firebaseapp.com",
-  projectId: "carbonemissionanalysis",
-  storageBucket: "carbonemissionanalysis.firebasestorage.app",
-  messagingSenderId: "730536626392",
-  appId: "1:730536626392:web:046cc4cf6b028a22efefad",
-  measurementId: "G-399Q82YSN7"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
